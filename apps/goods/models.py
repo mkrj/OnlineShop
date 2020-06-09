2da9bd2b83cd88477878982ead8afa275946740e
yangfang <2294240112@qq.com>
2020-06-09 16:49:22 +0800

    Added model Banner

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -111,6 +111,23 @@ class GoodsImage(models.Model):
         return self.goods.name
 
 
+class Banner(models.Model):
+    """
+    轮播的商品
+    """
+    goods = models.ForeignKey(Goods, verbose_name='商品', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='banner', verbose_name='轮播图片')
+    index = models.IntegerField(default=0, verbose_name='轮播顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '轮播商品'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.goods.name
+
+
 class HotSearchWords(models.Model):
     """
     热搜词
